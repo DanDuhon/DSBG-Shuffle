d88da7c8e442f6b5552fc07b8e4740771658b8d8
DanDuhon <daniel.e.duhon@gmail.com>
2023-12-22 16:02:14 -0600

    Scrollbar working!

--- lib/enemy_swapper.py
+++ lib/enemy_swapper.py
@@ -213,6 +213,68 @@ try:
                 adapter.exception(e)
                 raise
 
+    class VerticalScrolledFrame(ttk.Frame):
+        """A pure Tkinter scrollable frame that actually works!
+        * Use the "interior" attribute to place widgets inside the scrollable frame.
+        * Construct and pack/place/grid normally.
+        * This frame only allows vertical scrolling.
+        """
+        def __init__(self, parent, *args, **kw):
+            ttk.Frame.__init__(self, parent, *args, **kw)
+
+            # Create a canvas object and a vertical scrollbar for scrolling it.
+            vscrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL)
+            vscrollbar.pack(fill=tk.Y, side=tk.RIGHT, expand=tk.FALSE)
+            self.canvas = tk.Canvas(self, bd=0, highlightthickness=0,
+                            yscrollcommand=vscrollbar.set)
+            self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.TRUE)
+            vscrollbar.config(command=self.canvas.yview)
+
+            # Reset the view
+            self.canvas.xview_moveto(0)
+            self.canvas.yview_moveto(0)
+
+            # Create a frame inside the canvas which will be scrolled with it.
+            self.interior = interior = ttk.Frame(self.canvas)
+            interior_id = self.canvas.create_window(0, 0, window=interior, anchor=tk.NW)
+            self.interior.bind("<Enter>", self._bound_to_mousewheel)
+            self.interior.bind("<Leave>", self._unbound_to_mousewheel)
+            self.interior.bind("<Configure>", lambda event, canvas=self.interior: self.on_frame_configure(canvas))
+
+            # Track changes to the canvas and frame width and sync them,
+            # also updating the scrollbar.
+            def _configure_interior(event):
+                # Update the scrollbars to match the size of the inner frame.
+                size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
+                self.canvas.config(scrollregion="0 0 %s %s" % size)
+                if interior.winfo_reqwidth() != self.canvas.winfo_width():
+                    # Update the canvas"s width to fit the inner frame.
+                    self.canvas.config(width=interior.winfo_reqwidth())
+            interior.bind("<Configure>", _configure_interior)
+
+            def _configure_canvas(event):
+                if interior.winfo_reqwidth() != self.canvas.winfo_width():
+                    # Update the inner frame"s width to fill the canvas.
+                    self.canvas.itemconfigure(interior_id, width=self.canvas.winfo_width())
+            self.canvas.bind("<Configure>", _configure_canvas)
+
+
+        def on_frame_configure(self, canvas):
+            """Reset the scroll region to encompass the inner frame"""
+            canvas.configure(scrollregion=canvas.bbox("all"))
+
+
+        def _bound_to_mousewheel(self, event):
+            self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
+
+
+        def _unbound_to_mousewheel(self, event):
+            self.canvas.unbind_all("<MouseWheel>")
+
+
+        def _on_mousewheel(self, event):
+            self.canvas.yview_scroll(int(-1*(event.delta/120)), "units")
+
 
     class SettingsWindow(object):
         """
@@ -251,14 +313,14 @@ try:
                 self.notebook = ttk.Notebook(top)
                 self.notebook.grid(row=0, column=0, padx=(20, 10), pady=(20, 10), sticky="nsew", rowspan=4, columnspan=2)
 
-                self.expansionTab = ttk.Frame(self.notebook)
+                self.expansionTab = VerticalScrolledFrame(self.notebook)
                 self.notebook.add(self.expansionTab, text="Enabled Expansions")
 
-                self.expansionsScrollbar = ttk.Scrollbar(self.expansionTab)
-                self.expansionsScrollbar.grid(row=0, column=3, rowspan=12, sticky=tk.N+tk.S+tk.W)
+                # self.expansionsScrollbar = ttk.Scrollbar(self.expansionTab)
+                # self.expansionsScrollbar.grid(row=0, column=3, rowspan=12, sticky=tk.N+tk.S+tk.W)
                 for i, expansion in enumerate(self.expansions):
                     self.expansions[expansion]["value"].set(1 if expansion in self.settings["availableExpansions"] else 0)
-                    self.expansions[expansion]["button"] = ttk.Checkbutton(self.expansionTab, text=self.expansions[expansion]["displayName"], variable=self.expansions[expansion]["value"])
+                    self.expansions[expansion]["button"] = ttk.Checkbutton(self.expansionTab.interior, text=self.expansions[expansion]["displayName"], variable=self.expansions[expansion]["value"])
                     self.expansions[expansion]["button"].grid(row=i, column=0, padx=5, pady=10, sticky="nsew")
 
                 self.enemies = {
@@ -311,20 +373,17 @@ try:
                     "Falchion Skeleton (V1)": {"button": None, "value": tk.IntVar(), "parent": "Executioner Chariot (V1)", "children": [], "displayName": "Falchion Skeleton (V1)"}
                 }
 
-                self.enemiesTab = ttk.Frame(self.notebook)
+                self.enemiesTab = VerticalScrolledFrame(self.notebook)
                 self.enemiesTab.grid_propagate(False)
                 self.notebook.add(self.enemiesTab, text="Enabled Enemies")
-
-                self.enemiesScrollbar = ttk.Scrollbar(self.enemiesTab)
-                self.enemiesScrollbar.grid(row=0, column=4, rowspan=16, sticky=tk.N+tk.S+tk.W)
                 
                 for i, enemy in enumerate(self.enemies):
                     if self.enemies[enemy]["parent"]:
                         self.enemies[self.enemies[enemy]["parent"]]["children"].append(enemy)
                     else:
-                        tk.Label(self.enemiesTab, text="     ").grid(column=0, row=i)
+                        tk.Label(self.enemiesTab.interior, text="     ").grid(column=0, row=i)
 
-                    self.enemies[enemy]["button"] = ttk.Checkbutton(self.enemiesTab, text=self.enemies[enemy]["displayName"], variable=self.enemies[enemy]["value"], command=lambda enemy=enemy: self.toggle_parent_children(enemy=enemy))
+                    self.enemies[enemy]["button"] = ttk.Checkbutton(self.enemiesTab.interior, text=self.enemies[enemy]["displayName"], variable=self.enemies[enemy]["value"], command=lambda enemy=enemy: self.toggle_parent_children(enemy=enemy))
                     self.enemies[enemy]["button"].grid(row=i, column=0 if not self.enemies[enemy]["parent"] else 1, columnspan=2 if not self.enemies[enemy]["parent"] else 3, padx=5, pady=1, sticky="nsew")
 
                 for enemy in self.enabledEnemies:
