725eef7cfe7b233f751b0ea464659d3a4251e953
DanDuhon <daniel.e.duhon@gmail.com>
2024-07-18 18:53:34 -0500

    New variants

--- lib/dsbg_shuffle_variants.py
+++ lib/dsbg_shuffle_variants.py
@@ -131,7 +131,7 @@ try:
             self.diffLabel = ttk.Label(self.variantsTabButtonsFrame, text="Difficulty Modifier: +")
             self.diffLabel.pack(side=tk.LEFT, anchor=tk.CENTER, pady=5)
             self.entryText = tk.StringVar()
-            self.diffEntry = ttk.Entry(self.variantsTabButtonsFrame, textvariable=self.entryText, width=5, validate="all", validatecommand=(vcmd, "%P"))
+            self.diffEntry = ttk.Entry(self.variantsTabButtonsFrame, textvariable=self.entryText, width=6, validate="all", validatecommand=(vcmd, "%P"))
             self.diffEntry.pack(side=tk.LEFT, anchor=tk.CENTER, pady=5)
             self.diffLabel2 = ttk.Label(self.variantsTabButtonsFrame, text="%")
             self.diffLabel2.pack(side=tk.LEFT, anchor=tk.CENTER, pady=5)
@@ -728,7 +728,7 @@ try:
                             self.app,
                             "variants",
                             "variants",
-                            onlyDisplay=self.app.display,
+                            onlyDisplay=self.app.displayTopLeft,
                             name=child[:child.index(" - ")] if " - " in child else child[:child.index("_")] if "_" in child else child)
                 elif start == "All":
                     progressMax = 0
@@ -1298,9 +1298,9 @@ try:
                 
                 tree = self.treeviewVariantsList
 
-                for display in [self.app.display, self.app.displayTopRight, self.app.displayBottomRight]:
+                for display in [self.app.displayTopLeft, self.app.displayBottomLeft, self.app.displayTopRight, self.app.displayBottomRight]:
                     display.config(image="")
-                    self.app.displayTopLeft.image=None
+                    display.image=None
                     self.app.displayImages["variants"][display]["image"] = None
                     self.app.displayImages["variants"][display]["name"] = None
                     self.app.displayImages["variants"][display]["activeTab"] = None
@@ -2224,7 +2224,7 @@ try:
             try:
                 log("Start of callback")
 
-                if (str.isdigit(P) or str(P) == "") and len(P) <= 5:
+                if (str.isdigit(P) or str(P) == "") and len(P) <= 6:
                     log("End of callback")
                     return True
                 else:
